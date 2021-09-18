3894cc8b69da4a3f0862a1208e9d7267e2acd0a7
John Sedor <john.sedor08@gmail.com>
2021-09-18 17:59:16 -0400

    Initial revision.

---
+++ src/main/java/com/example/udp/springboot/UDPApplication.java
@@ -0,0 +1,12 @@
+package com.example.udp.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UDPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UDPApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/udp/springboot/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.udp.springboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
